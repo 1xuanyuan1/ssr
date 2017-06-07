65d9ab014801752b07209915ce652df263d0f123
Duke <546407233@qq.com>
2017-06-07 12:59:46 +0800

    修改部分配置

--- template/src/app.js
+++ template/src/app.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import 'es6-promise/auto'
 import App from './App.vue'
 import { createRouter } from './router'
 
